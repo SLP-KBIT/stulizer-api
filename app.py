06e595b753698ed304a38b50ba93dab1cfbe1842
amakuchi <s18t318@stu.kagawa-u.ac.jp>
2020-07-11 20:43:28 +0900

    take measures to cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import *
+from flask import Flask, jsonify
+from flask_cors import CORS
 from cardinfo import CardInfo
 from connection import Connection
 
 app = Flask(__name__)
+CORS(app)
 app.config["JSON_AS_ASCII"] = False
 
 @app.route("/")
